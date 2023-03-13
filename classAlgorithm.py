04c8f2338ec7673cb78ac2bbb3c7eba0172b19b2
fadingNA <94852295+fadingNA@users.noreply.github.com>
2023-03-13 13:14:07 -0400

    Add find closest

--- classAlgorithm.py
+++ classAlgorithm.py
@@ -1,3 +1,5 @@
+import math
+
 # Classic ALgorithms
 
 """
@@ -76,6 +78,69 @@ def printList(arr):
 		print(arr[i] , end = " ")
 	print()
 
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+def compare_x(a,b):
+	p1 = a
+	p2 = b
+	return (p1.x - p2.x)
+
+def compare_y(a,b):
+	p1 = a 
+	p2 = b 
+	return (p1.y - p2.y)
+
+def dist(p1, p2):
+	return math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)))
+
+def bruteForce(P, n):
+	min_dist = float("inf")
+	for i in range(n):
+		for j in range(i + 1, n):
+			if dist(P[i], P[j])< min_dist:
+				min_dist = dist(P[i],P[j])
+	return min_dist
+
+def min(x,y):
+	return x if x < y else y
+
+def stripClosest(strip, size, d):
+	min_dist = d
+	strip = sorted(strip, key=lambda point: point.y)
+
+	for i in range(size):
+		for j in range(i + 1, size):
+			if strip[j].y - strip[i].y >= min_dist:
+				break
+			if dist(strip[i], strip[j]) < min_dist:
+				min_dist = dist(strip[i], strip[j])
+	return min_dist
+
+
+def clossetUtill(P,n):
+	if n <= 3:
+		return bruteForce(P,n)
+
+	mid = n // 2
+	midPoint = P[mid]
+	dl = clossetUtill(P,mid)
+	dr = clossetUtill(P[mid:], n - mid)
+	d = min(dl,dr)
+	strip = []
+	for i in range(n):
+		if abs(P[i].x - midPoint.x) < d:
+			strip.append(P[i])
+	return min(d, stripClosest(strip, len(strip), d))
+
+def closest(P, n):
+    P = sorted(P, key=lambda point: point.x)
+    return closestUtil(P, n)
+
+
+
 printList(test_bubble)
 
 if __name__ == '__main__':
@@ -85,3 +150,8 @@ if __name__ == '__main__':
 	merge_sort(arr)
 	print("Sroted array is: ", end="\n")
 	printList(arr)
+
+	P = [Point(x=2, y=3), Point(x=12, y=30),
+         Point(x=40, y=50), Point(x=5, y=1), Point(x=12, y=10), Point(x=3, y=4)]
+    n = len(P)
+    print("The smallest distance is", closest(P, n))
